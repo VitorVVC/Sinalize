5ffeb2811464eb9b1728c2b55939817aedc19692
Vitor Vargas Cardoso <144281355+VitorVVC@users.noreply.github.com>
2024-08-29 08:21:27 -0300

    Add EnvInt function

--- utils/env.go
+++ utils/env.go
@@ -6,6 +6,7 @@ import (
 	"go.uber.org/zap"
 	"log"
 	"os"
+	"strconv"
 )
 
 var Defaults = map[string]interface{}{
@@ -42,5 +43,29 @@ func EnvString(key string) string {
 		zap.L().Fatal("wrong type", zap.Error(ErrWrongEnvType), zap.String("env", key))
 	}
 
+	return value
+}
+
+func EnvInt(key string) int64 {
+	valueStr := os.Getenv(key)
+	value, err := strconv.ParseInt(valueStr, 10, 64)
+	if valueStr == "" || err != nil {
+		if err != nil && valueStr != "" {
+			zap.L().Panic("failed to convert integer from env", zap.Error(err))
+		}
+
+		valueInterface, ok := Defaults[key]
+		if !ok {
+			zap.L().Fatal("missing env", zap.Error(ErrMissingEnv), zap.String("env", key))
+		}
+
+		valueInt, ok := valueInterface.(int)
+		if !ok {
+			zap.L().Fatal("wrong type", zap.Error(ErrWrongEnvType), zap.String("env", key))
+		}
+		value = int64(valueInt)
+	}
+
+	//zap.L().Debug("returned env", zap.String("key", key), zap.Int64("value", value))
 	return value
 }
