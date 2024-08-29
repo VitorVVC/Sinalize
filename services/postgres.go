597dc49acc9784d2cfd3b64b5528095712cb2637
Vitor Vargas Cardoso <144281355+VitorVVC@users.noreply.github.com>
2024-08-29 08:19:56 -0300

    Add services to Postgres

---
+++ services/postgres.go
@@ -0,0 +1,35 @@
+package services
+
+import (
+	"SinalizeB/constants"
+	"SinalizeB/utils"
+	"fmt"
+
+	"go.uber.org/zap"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var postgresInstance *gorm.DB
+
+func Postgres() *gorm.DB {
+	if postgresInstance == nil {
+		connectionURL := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=%s",
+			utils.EnvString(constants.PostgresHost),
+			utils.EnvString(constants.PostgresUser),
+			utils.EnvString(constants.PostgresPass),
+			utils.EnvString(constants.PostgresName),
+			utils.EnvInt(constants.PostgresPort),
+			"disable",
+		)
+
+		db, err := gorm.Open(postgres.Open(connectionURL))
+		if err != nil {
+			zap.L().Panic("failed to connect to postgres", zap.Error(err))
+		}
+
+		postgresInstance = db
+	}
+
+	return postgresInstance
+}
